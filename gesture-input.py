d5a5b1b20c8150e24b2603a6778155529961a46d
tina-e <tina.emmert@web.de>
2023-06-15 23:15:18 +0200

    add task 1

--- gesture-input.py
+++ gesture-input.py
@@ -1,1 +1,44 @@
 # gesture input program for first task
+
+import sys
+import pyglet
+from recognizer import OneDollarRecognizer
+
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500
+
+window = pyglet.window.Window(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+result_text = pyglet.text.Label("???", x=WINDOW_WIDTH-50, y=WINDOW_HEIGHT-20, anchor_x='center', anchor_y='center')
+points = []
+
+recognizer = OneDollarRecognizer()
+
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == pyglet.window.key.SPACE:
+        points.clear()
+    elif symbol == pyglet.window.key.Q:
+        sys.exit(0)
+
+
+@window.event
+def on_mouse_release(x, y, button, modifiers):
+    window.clear()
+    if button & pyglet.window.mouse.LEFT:
+        result = recognizer.recognize(points)
+        result_text.text = result[0]
+        print(result)
+        points.clear()
+        result_text.draw()
+
+
+@window.event
+def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
+    if buttons & pyglet.window.mouse.LEFT:
+        points.append(recognizer.get_point(int(x), int(WINDOW_HEIGHT-y)))
+        rect = pyglet.shapes.Line(x, y, x+dx, y+dy, width=3)
+        rect.draw()
+
+
+pyglet.app.run()
--- recognizer.py
+++ recognizer.py
@@ -1,1 +1,35 @@
 # $1 gesture recognizer
+
+import os
+import xml.etree.ElementTree as ET
+from dollarpy import Recognizer, Template, Point
+
+
+class OneDollarRecognizer:
+
+    def __init__(self):
+        self.gestures = ['arrow', 'caret', 'circle', 'check', 'rectangle']
+        self.recognizer = self.read_data_and_define_recognizer()
+
+    def read_data_and_define_recognizer(self):
+        #gesture_data = dict()
+        templates = []
+        for gest in self.gestures:
+            tree = ET.parse(f'dataset/s01/slow/{gest}01.xml')
+            root = tree.getroot()
+            points = []
+            for point in root:
+                points.append(Point(int(point.attrib['X']), int(point.attrib['Y'])))
+
+                # x = point.attrib['X']
+                # y = point.attrib['Y']
+                # print(x, y)
+                # gesture_data[gest] = point.attrib
+            templates.append(Template(gest, points))
+        return Recognizer(templates)
+
+    def get_point(self, x, y):
+        return Point(x, y)
+
+    def recognize(self, point_list):
+        return self.recognizer.recognize(point_list)
